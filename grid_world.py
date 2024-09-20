cb676ee3139071eb5b98dc543dffe59a8c4302a9
schlagem <schlagem@informatik.uni-freiburg.de>
2024-09-20 16:25:41 +0200

    linting

--- grid_world.py
+++ grid_world.py
@@ -45,7 +45,7 @@ class GridWorld(gym.Env):
             for k in range(len(self.world[0])):
                 if np.array_equal(self.state, [k, i]):
                     string += "@"
-                elif self.world[i][k] ==  -10:
+                elif self.world[i][k] == -10:
                     string += "X"
                 elif self.world[i][k] == 10:
                     string += "$"
